5c7c00de552493bfbaf17660e4062db08e418ae9
Restart1 <eladh@restrtit.co.il>
2017-09-06 13:40:48 +0300

    change name from sink to event for consisnt naming

--- js/pubSubRxJs.js
+++ js/pubSubRxJs.js
@@ -1,39 +1,39 @@
 // https://medium.com/@benlesh/on-the-subject-of-subjects-in-rxjs-2b08b7198b93
 function PubSub(){
 
-    let sinks = {};
+    let event = {};
 
-    function publish(event, data) {
-        if (!sinks[event]) {
-            console.warn(`${event} does not have any registered listeners. Publish cancelled.`);
+    function publish(eventName, data) {
+        if (!event[eventName]) {
+            console.warn(`${eventName} does not have any registered listeners. Publish cancelled.`);
             return false;
         }
 
-        //return this.sinks[event].next(data);//rxjs 5
-        return sinks[event].onNext(data);
+        return event[eventName].next(data);//rxjs 5
+        //return event[eventName].onNext(data); //rx 4
     }
 
-    function subscribe(event, cb) {
+    function subscribe(eventName, cb) {
         if (!cb) {
             console.warn('No callback provided for subscription. You must implement subscribe for yourself.');
         }
 
-        if (!sinks[event]) {
-            sinks[event] = new Rx.Subject();
+        if (!event[eventName]) {
+            event[eventName] = new Rx.Subject();
         }
 
-        return cb ?  sinks[event].subscribe(cb) : sinks[event];
+        return cb ?  event[eventName].subscribe(cb) : event[eventName];
     }
 
     function destroy() {
-        for (const s in sinks) {
-            if (sinks.hasOwnProperty(s)) {
-                //this.sinks[s].unsubscribe(); //rxjs 5
-                sinks[s].dispose();
+        for (const s in event) {
+            if (event.hasOwnProperty(s)) {
+                event[s].unsubscribe(); //rxjs 5
+                //event[s].dispose(); //rx 4
             }
         }
 
-        sinks = {};
+        event = {};
     }
 
     return {
